f8f0c3ddc252d7a15b9f82f2f17e26003bbd0071
Andeli1 <wanjikuanni045@gmail.com>
2024-05-08 23:10:01 +0300

    "new changes"

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 let originalColor = '#fofofo';
-
+// Previous background color
+let prevColor = originalColor;
+// Function to change background color
+function changeBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+    prevColor = originalColor;
+    originalColor = color;
+  }
+  // Event listener for button clicks
+document.querySelectorAll('button').forEach(item => {
+    item.addEventListener('click', event => {
+      changeBackgroundColor(event.target.style.backgroundColor);
+    });
+  });
+  
 
 
